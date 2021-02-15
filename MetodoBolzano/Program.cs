2ca5a7ff8b82a3abdcab835b2bde9585dbbb004c
Humberto <hgerardop01@gmail.com>
2021-02-14 21:42:47 -0600

    Agregar archivos de proyecto.

---
+++ MetodoBolzano/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MetodoBolzano
+{
+    class Program
+    {
+            
+        static void Main(string[] args)
+        {
+            Boolean flag = true;
+            int numIt = 0;
+            float x1, x2, xr_prev, xr, errorDes, errAprox;
+            Console.WriteLine("===> Bienvenido al Método Bolzano Para Funciones Cuadradas <===\n");
+            metods met = new metods();
+            met.obtencionDatos();
+            errorDes = met.obtencionError();
+            //Paso 1. Valores Iniciales
+            do
+            {
+            Console.WriteLine("Ingrese el valor inicial x1: ");
+            x1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor inicial xv: ");
+            x2 = Convert.ToSingle(Console.ReadLine());
+            }while(met.inicVal(x1, x2));
+            //Paso 2. Aproximación a la Raíz
+            xr = met.midPoint(x1,x2);
+
+            //Paso 3. Evaluar y Determinar el Caso
+            do
+            {
+                numIt += 1;
+                switch (met.evRaices(x1, x2))
+                {
+                    case 1:
+                        x2 = xr;
+                        break;
+                    case 2:
+                        x1 = xr;
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+
+                //Paso 4. Nueva Aproximación a la Raiz
+                xr_prev = xr;
+                xr = met.midPoint(x1, x2);
+
+                //Paso 5. Verificar la Exactitud de la Aproximación
+                errAprox = met.errorAproximado(xr, xr_prev);
+                if ( errAprox <= errorDes)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("===> Reporte de Iteración #{0} <===", numIt);
+                    Console.WriteLine("Porcentaje de error deseado: {0}%\n", errorDes);
+                    Console.WriteLine("Valor de la raíz aproximada: {0}\n", xr);
+                    Console.WriteLine("Error aproximado: {0}\n", errAprox);
+                    Console.WriteLine("Valor Final de x_1: {0}\nValor Final de x_v: {1}\n", x1, x2);
+                    Console.ReadLine();
+                }
+            } while (flag);
+            Console.WriteLine("===> Reporte de Finela Iteración #{0} <===", numIt);
+            Console.WriteLine("Porcentaje de error deseado: {0}%\n", errorDes);
+            Console.WriteLine("Valor de la raíz aproximada: {0}\n", xr);
+            Console.WriteLine("Error aproximado: {0}\n", errAprox);
+            Console.WriteLine("Valor Final de x_1: {0}\nValor Final de x_v: {1}\n", x1, x2);
+            Console.ReadLine();
+
+        }
+        
+                
+    }
+}
---
+++ MetodoBolzano/metods.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodoBolzano
+{
+    class metods
+    {
+        public float x2, x1, x;
+        public metods() { }
+
+        //Obtención de datos
+        public void obtencionDatos()
+        {
+            //  Coeficientes de la función
+            Console.WriteLine("Escribe el coeficiente de x^2: ");
+            x2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Escribe el coeficiente de x: ");
+            x1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Escribe el coeficiente independiente: ");
+            x = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("La función es: {0}x^2+{1}x+{2}", x2, x1, x);
+        }
+
+        //Obtener Error Deseado
+        public float obtencionError()
+        {
+            Boolean bandera;
+            float err;
+            Console.WriteLine("Ingrese el valor del error deseado en el rango de 0% - 100%: ");
+            do
+            {
+                err = Convert.ToSingle(Console.ReadLine());
+                if (err < 100 || err > 0)
+                {
+                    bandera = false;
+                }
+                else
+                {
+                    Console.WriteLine("El valor del error deseado no está dentro del rango, ingrese el valor del error deseado de nuevo: ");
+                    bandera = true;
+                }
+            } while (bandera);
+            return err;
+        }
+
+        //Verificar los valores iniciales
+        public Boolean inicVal(float x1, float x2)
+        {
+            if (((this.x2 * Math.Pow(x1, 2)) + this.x1 * x1 + this.x) * ((this.x2 * Math.Pow(x2, 2)) + this.x1 * x2 + this.x) < 0)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Los valores iniciales no son correctas, verifiquelos, porfavor.\n");
+                return true;
+            }
+        }
+
+        //  Punto Medio
+        public float midPoint(float x1, float x2)
+        {
+
+            return (x1 + x2) / 2;
+        }
+
+        //  Determinar el caso
+        public int evRaices(float x1, float xr)
+        {
+            float _valorProducto = ((this.x2 * Convert.ToSingle(Math.Pow(x1, 2))) + this.x1 * x1 + this.x) * ((this.x2 * Convert.ToSingle(Math.Pow(xr, 2))) + this.x1 * xr + this.x);
+            if (_valorProducto < 0)
+            {
+                return 1;
+            }
+            else if (_valorProducto > 0)
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+        }
+
+        public float errorAproximado(float vaAct, float vaPrev)
+        {
+            return Math.Abs((vaAct - vaPrev) / vaAct) * 100;
+        }
+
+    }
+}
